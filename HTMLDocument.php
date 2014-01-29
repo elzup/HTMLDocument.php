65116f9b08c5b706199850f7498925936feb799c
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-01-29 21:49:37 +0900

    make createTagA

--- HTMLDocument.php
+++ HTMLDocument.php
@@ -7,17 +7,24 @@ class HTMLDocument extends DOMDocument {
 		$this->createElement($name);
 	}
 
-	function createTag($name, $value, array $attributes, $id, array $classes, array $styles) {
+	function createTag($name, $value = null, array $attributes = null, $id = null, array $classes = null, array $styles = null) {
 		$tag = $this->createElement($name, $value);
-		foreach ($attibutes as $name => $value)
-			$tag->setAttribute($name, $value);
-		$tag->setAttribute('id', $id);
-		$tag->setAttribute('class', implode(' ', $classes));
-		$tag->setAttribute('style', implode_associative('', $styles, '%s: %s;'));
+		if (isset($attributes))
+			foreach ($attibutes as $name => $val)
+				$tag->setAttribute($name, $val);
+		if (isset($id)) $tag->setAttribute('id', $id);
+		if (isset($classes)) $tag->setAttribute('class', implode(' ', $classes));
+		if (isset($styles)) $tag->setAttribute('style', implode_associative('', $styles, '%s: %s;'));
+		return $tag;
+	}
+
+	function createTagA($name, $value = null, $href, array $attributes = null, $id = null, array $classes = null, array $styles = null) {
+		$attributes['href'] = $href;
+		return $this->createTag($name, $value, $attributes, $id, $classes, $styles);
 	}
 }
 
-function implode_format($glue, $pieces, $format = '(%s => %s)') {
+function implode_format($glue, $pieces, $format = '[%s] => %s') {
 	$array = array();
 	foreach ($pieces as $key => $value) {
 		$array[] = sprintf($format, $key, $value);
@@ -26,11 +33,11 @@ function implode_format($glue, $pieces, $format = '(%s => %s)') {
 }
 
 /* function implode_associative($glue, $inside_glue, $pieces) {
-	$array = array();
-	foreach ($pieces as $key => $value) {
-		$array[] = $key . $inside_glue . $pieces;
-	}
-	return implode($glue, $array);
+ $array = array();
+foreach ($pieces as $key => $value) {
+$array[] = $key . $inside_glue . $pieces;
+}
+return implode($glue, $array);
 } */
 
 
