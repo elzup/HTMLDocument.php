8036f20e6043e8321bdc718072e41afa27983785
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-01-29 17:01:36 +0900

    create Tag method

--- HTMLDocument.php
+++ HTMLDocument.php
@@ -2,14 +2,37 @@
 
 class HTMLDocument extends DOMDocument {
 
-	/**
-	 * Creates a new HTMLDocument object
-	 * @param string version[optional]
-	 * @param string encoding[optional]
-	 */
 	function __construct($version, $encoding) {
 		parent::__construct($version, $encoding);
+		$this->createElement($name);
+	}
+
+	function createTag($name, $value, array $attributes, $id, array $classes, array $styles) {
+		$tag = $this->createElement($name, $value);
+		foreach ($attibutes as $name => $value)
+			$tag->setAttribute($name, $value);
+		$tag->setAttribute('id', $id);
+		$tag->setAttribute('class', implode(' ', $classes));
+		$tag->setAttribute('style', implode_associative('', $styles, '%s: %s;'));
 	}
 }
+
+function implode_format($glue, $pieces, $format = '(%s => %s)') {
+	$array = array();
+	foreach ($pieces as $key => $value) {
+		$array[] = sprintf($format, $key, $value);
+	}
+	return implode($glue, $array);
+}
+
+/* function implode_associative($glue, $inside_glue, $pieces) {
+	$array = array();
+	foreach ($pieces as $key => $value) {
+		$array[] = $key . $inside_glue . $pieces;
+	}
+	return implode($glue, $array);
+} */
+
+
 
 ?>
